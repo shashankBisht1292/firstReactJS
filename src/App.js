6ca1320c5b6e97f478fe0e9258decbb435e4b3ae
shashank bisht <shashankbishtgehu@gmail.com>
2019-10-01 23:59:37 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Person from './Person/Person';
+
+class App extends Component {
+  render() {
+    const name = 'somil';
+    return (
+      <div className="App">
+        <h1>Hi {name}, i am a react app !</h1>
+        <p>this is really working</p>
+        <Person name='Max' age='10'/>
+        <Person name='Manu' age='30'/>
+        <Person name='Steph' age='60'>Hobbies is basketball</Person>
+        <Person name='somil' age='20'/>
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, 'h1', 'Hi, I\'m areact app!!!');
+  }
+}
+
+export default App;
